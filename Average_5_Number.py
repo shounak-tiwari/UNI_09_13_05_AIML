51b60819b7c1b27dca4f5baae0305a9051bb5ef9
Akash Tiwari <shounaktiwari@gmail.com>
2026-05-20 09:32:38 +0530

    first commit

---
+++ Average_5_Number.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the number 1 : "))
+num2 = int(input("Enter the number 2 : "))
+num3 = int(input("Enter the number 3 : "))
+num4 = int(input("Enter the number 4 : "))
+num5 = int(input("Enter the number 5 : "))
+add = num1+num2+num3+num4+num5
+print("Average  = (Sum of all observation / number of observation) : ",(add/5))
---
+++ Sum_Two_Number.py
@@ -0,0 +1,4 @@
+X = 10 
+Y = 20
+SUM = X+Y
+print("The sum of x and y : ",SUM)
